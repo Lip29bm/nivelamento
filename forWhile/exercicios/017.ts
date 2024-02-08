5cd86bdecf02b8fa054b63d00c866a85246adbc8
Nicoly Filbert Ortiz <colyfilbert@Nicolys-Mac-mini.local>
2024-02-08 09:51:56 -0300

    Exercícios 17 e 18 refatorados

--- forWhile/exercicios/017.ts
+++ forWhile/exercicios/017.ts
@@ -1,16 +1,20 @@
-let mediaEscolar:{nome:string,media:number}[]=[]
-let nomes: string[]=["carla","joao","marcos","lucia","eduardo"]
-let quantiaDeAlunos:number=0
-let mediaIndividual:number[]=[4.5,8,5.4,9,2.8,]
-let mediaTotal:number=0
-for(let i :number=0;i<=nomes.length && i<=mediaIndividual.length;i++){
-    if(nomes[i] === "fim"){
-        break;
-    }
-    quantiaDeAlunos++
-    mediaTotal+=mediaIndividual[i]
-mediaEscolar.push({nome:nomes[i], media:mediaIndividual[i]})
+import readline from "readline-sync"
 
+function medias(mediaEscolar:number, quantia:number, somaDasNotasIndividuais:number):void{
+    let i:number = 0
+    while(i <20 ){
+        let nome:string = String(readline.question(("Nome do aluno:")))
+        if(nome == "fim"){
+            break
+        }
+        let mediaDoAluno:number = Number(readline.question(("Media do aluno:")))
+            quantia ++
+            somaDasNotasIndividuais += mediaDoAluno
+        
+        
+    }
+    mediaEscolar = somaDasNotasIndividuais / quantia
+    console.log("A media escolar foi " + mediaEscolar)
+    console.log("A quantidades de alunos foram " + quantia)
 }
-console.log(quantiaDeAlunos)
-console.log(mediaTotal)
+medias(0, 0, 0)
--- forWhile/exercicios/018.ts
+++ forWhile/exercicios/018.ts
@@ -1,7 +1,7 @@
-
-let polegada:number = 2.54
-
+function transformarPolegada(polegada:number ){ 
 for(let i:number= 0; i <= 20; i++){
     let resultado: number = polegada * i
     console.log(resultado)
 }
+}
+transformarPolegada(2.54)
